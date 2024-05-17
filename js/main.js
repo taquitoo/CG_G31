c2a6dd0b1884e4312fd2e18b3f183651117f0623
Pedro Vieira <pedroperezvieira@tecnico.ulisboa.pt>
2024-05-17 15:34:06 +0100

    Ex4: Global lighting added Provided a method for adding the surfaces to the carousel.

--- js/main.js
+++ js/main.js
@@ -1,44 +1,33 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.121.1/build/three.module.js';
 import { max, varyingProperty } from 'three/examples/jsm/nodes/Nodes.js';
-/*
 import {
   OrbitControls
 } from 'https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/OrbitControls.js';
-*/
 
 const ROTATION_SPEED = Math.PI/1440;
+const RADIAL_SEGMENTS = 64;
 
-var scene, renderer; 
-var camera;
+var scene, renderer, camera; 
+var directionalLight, ambientLight;
 
 var carousel;
 
 var keys = {};
 var pressedKeys = [];
 
-var materialDefault = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true});
-//var materialDefault = new THREE.MeshPhongMaterial();
+//var materialDefault = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true});
+var materialDefault = new THREE.MeshPhongMaterial({color: 0xFFD0D0});
 
-var counter = [0, 0, 0, 0];
+var counter = [0, Math.PI/2, Math.PI/2, Math.PI/2];
 
 function addCarouselBase(obj, x, y, z, radius, height) {
-    var geometry = new THREE.CylinderGeometry(radius, radius, height, 32);
+    var geometry = new THREE.CylinderGeometry(radius, radius, height, RADIAL_SEGMENTS);
     var mesh = new THREE.Mesh(geometry, materialDefault);
     mesh.position.set(x, y, z);
     obj.add(mesh);
 }
 
 function addCarouselRing(obj, x, y, z, innerRadius, outerRadius) {
-	/*
-	const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444, 0.4);
-	hemiLight.position.set(0, 150, 0);
-	scene.add(hemiLight);
-
-	const dirLight = new THREE.DirectionalLight(0xffffff, 0.6);
-	dirLight.position.set(-130, 150, 150);
-	scene.add(dirLight);
-*/
-
 	const width = 100;
 	const height = outerRadius - innerRadius;
 	const radius = innerRadius + (outerRadius - innerRadius)/2
@@ -59,7 +48,7 @@ function addCarouselRing(obj, x, y, z, innerRadius, outerRadius) {
 
 	const extrudeSettings = {
 		depth: 10,
-		steps: 32,
+		steps: RADIAL_SEGMENTS,
 		bevelEnabled: false,
 		extrudePath: path
 	};
@@ -68,7 +57,6 @@ function addCarouselRing(obj, x, y, z, innerRadius, outerRadius) {
 
 	const mesh = new THREE.Mesh(geometry, materialDefault);
 	obj.add(mesh);
-
 }
 
 class Carousel extends THREE.Object3D {
@@ -83,6 +71,10 @@ class Carousel extends THREE.Object3D {
 								Carousel.RING_WIDTH*(1+i),
 								Carousel.RING_WIDTH*(2+i));
 		}
+		this.surfaces_number = new Array(Carousel.N_RINGS+1);
+		for(var i = 0; i <= Carousel.N_RINGS; i++) {
+			this.surfaces_number[i] = 0;
+		}
 	}
 	setRingElevation(ring_number, percentage) {
 		const ring = this.children[ring_number];
@@ -91,19 +83,38 @@ class Carousel extends THREE.Object3D {
 			ring.position.y = (percentage+1)/2 * Carousel.RING_HEIGHT/2;
 		}
 	}
+	addSurface(ring_number, surface) {
+		if(this.surfaces_number[ring_number] < 8) {
+			let angle = Math.PI/4 * this.surfaces_number[ring_number];
+			surface.position.set(
+				Math.cos(angle) * Carousel.RING_WIDTH * (0.5+ring_number),
+				Carousel.RING_HEIGHT/2, 
+				Math.sin(angle) * Carousel.RING_WIDTH * (0.5+ring_number),
+			);
+			this.children[ring_number].add(surface);
+			this.surfaces_number[ring_number]++;
+		}
+	}
+}
+
+function createLighting() {
+	directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+	directionalLight.position.set(300, 1000, 200).normalize();
+	scene.add(directionalLight);
+
+	// orange-ish ambient light
+	ambientLight = new THREE.AmbientLight(0xFF6500, 0.5);
+	scene.add(ambientLight);
 }
 
 function createCamera() {
 	/* TODO initial camera to be able to see what I'm doing, not the one asked */
-    const aspectRatio = window.innerWidth / window.innerHeight; 
-    const frustumSize = 400;
-    camera = new THREE.OrthographicCamera(-frustumSize, frustumSize, frustumSize, -frustumSize, 1, 2000);
-    camera.position.set(800, 800, 800);
-    camera.lookAt(new THREE.Vector3(0, 190, 0));
-	/*
-	camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 100);
-	camera.position.z = 50;
-	*/
+	const aspectRatio = window.innerWidth / window.innerHeight;
+	const width = 400;
+	const height = width / aspectRatio;
+	camera = new THREE.OrthographicCamera(-width, width, height, -height, 1, 1000);
+	camera.position.set(200, 200, 200);
+	camera.lookAt(new THREE.Vector3(0, 0, 0));
 }
 
 function createScene() {
@@ -124,6 +135,9 @@ function update() {
 			counter[i] += 3.1;
 			carousel.setRingElevation(i, percentage);
 		}
+		for(var j=0; j<carousel.surfaces_number[i]; j++) {
+			carousel.children[i].children[j].rotation.y += ROTATION_SPEED*4;
+		}
 	}
 }
 
@@ -136,6 +150,12 @@ function animate() {
 function onKeyDown(e) {
 	e.preventDefault();
 	keys[e.key.toLowerCase()] = true;
+	switch(e.key) {
+		case 'd':
+		case 'D':
+			directionalLight.visible = !directionalLight.visible;
+			break;
+	}
 }
 
 function onKeyUp(e) {
@@ -146,16 +166,29 @@ function onKeyUp(e) {
 function init() {
 	createScene();
 	createCamera();
+	createLighting();
 
 	carousel = new Carousel(0, 0, 0);
     scene.add(carousel);
+
+	/* Example of how to add a surface to the carousel 
+	 * Use it as you need it inside your function to add the various surfaces
+	 * Take into account that the surfaces centroid is added to the floor of the ring,
+	 * so you have to have the referential of each surface at its bottom,
+	 * otherwise half of the surface will be inside the ring
+	 * */
+	for (var i=1; i<=8; i++) {
+		carousel.addSurface(1, new THREE.Mesh(new THREE.BoxGeometry(20, 20, 20).translate(0, 10, 0), materialDefault));
+		carousel.addSurface(2, new THREE.Mesh(new THREE.BoxGeometry(20, 20, 20).translate(0, 10, 0), materialDefault));
+		carousel.addSurface(3, new THREE.Mesh(new THREE.BoxGeometry(20, 20, 20).translate(0, 10, 0), materialDefault));
+	}
 
     renderer = new THREE.WebGLRenderer({
         antialias: true,
     });
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
-	//const controls = new OrbitControls(camera, renderer.domElement)
+	const controls = new OrbitControls(camera, renderer.domElement)
 
     window.addEventListener("keydown", onKeyDown);
     window.addEventListener("keyup", onKeyUp);
